ade0bec4753f9ea6744338e3d2dda18ac6966eaf
Zachary Hickey <zmhickey@Zacharys-MBP.local>
2020-08-26 17:38:37 -0500

    quiz for data

--- test.cpp
+++ test.cpp
@@ -2,12 +2,39 @@
 #include <iostream>
 
 using namespace std;
+
+int getValue (int);
 int main()
 {
-cout << "it workes";
-	for (int k=0; k<3;k++)
+/*	int n = 0;
+	cout <<"enter a number: ";
+	cin >> n;
+	int out=getValue(n);
+	cout <<endl<<out;
+*/
+for (int k=1; k <=1000; k++)
 {
-cout << k << endl;
+	int exp=0;
+	if ((k % 3) == 0)
+	  { cout<< "FIZZ";
+	exp++;}
+	if ((k % 5) == 0)
+	{
+	   cout<< "BUZZ";
+	exp++;}
+	if (exp==0)	
+	  cout << k << endl;
+	else cout <<endl;
 }
+		
 return 0;
 }
+
+int getValue(int n )
+{
+	
+	if (n<=1)
+	  return n;
+	return (getValue(n-1) + getValue(n-2));
+}
+
